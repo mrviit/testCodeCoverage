37680af4ef7eca273dc989b18dd44190df5aa0de
mrviit <mrviit@gmail.com>
2019-01-23 18:09:25 +0700

    Create Calculator.test.ts

---
+++ tests/unit/mocha/Calculator.test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+// tslint:disable-next-line:no-var-requires
+const Calculator = require("../../src/helper/Calculator");
+
+const cal = new Calculator();
+
+describe("calculate", () => {
+  it("add", () => {
+    const result = cal.Sum(5, 2);
+    expect(result).equal(7);
+  });
+
+  it("substract", () => {
+    const result = cal.Difference(5, 2);
+    expect(result).equal(3);
+  });
+});
